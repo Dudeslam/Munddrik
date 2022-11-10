5efe0167041906ed7c1e2bf28e9074b1534fde47
Vinh Trung <Trung_thai@live.dk>
2022-11-10 19:40:46 +0100

    It works Yet to be implemented as a standalone application work in progress

--- src/stores/MunddrikMSG.ts
+++ src/stores/MunddrikMSG.ts
@@ -1,10 +1,11 @@
 import { inject} from 'vue'
 import { defineStore } from 'pinia'
-
+import csv from './Munddrik.tsv'
 export interface msg{
   Message: string,
   AllMessage: string[],
-  Munddrik: string[]
+  Munddrik: string[],
+  first: boolean
 }
 const $papa = inject("$papa");
 export const useMunddrikStore = defineStore({
@@ -13,20 +14,28 @@ export const useMunddrikStore = defineStore({
     Message: "",
     AllMessage: [],
     Munddrik: ["Bund din drik!!!!", "Du vælger en som skal bunde!!!!"],
+    first: true
   }),
   actions:{
-    loadMovies(){
-      console.log("hello")
-      console.log($papa.parse("./Munddrik.csv"))
+    async loadMovies(){
+      await fetch('./Munddrik.txt').then(x => x.text)
+      csv.forEach(element => {
+        this.AllMessage.push(Object.values(element).toString())
+      });
+      const valuest = Object.values(csv)
+      console.log(this.AllMessage)
+      if(this.first){
+        this.roll()
+        this.first = false;
+      }
     },
     roll(){
       const rand = Math.round((Math.random()*this.AllMessage.length));
-      console.log(rand)
       this.Message = this.AllMessage[rand]
     },
     haeld(){
-      const rand = Math.round((Math.random()*100) % 2);
-      this.Message = this.Munddrik[rand];
+      let rand = Math.round((Math.random()*100)%2);
+      this.Message = this.Munddrik[rand ? 0 : 1];
     },
   },
 
