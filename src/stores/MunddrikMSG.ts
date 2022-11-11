cb7227ce59ab3d4e0e96db2d3207e462711febd5
Vinh Trung <Trung_thai@live.dk>
2022-11-11 19:18:00 +0100

    New entries

--- src/stores/MunddrikMSG.ts
+++ src/stores/MunddrikMSG.ts
@@ -1,11 +1,11 @@
-import { inject} from 'vue'
-import { defineStore } from 'pinia'
-import csv from './Munddrik.tsv'
-export interface msg{
-  Message: string,
-  AllMessage: string[],
-  Munddrik: string[],
-  first: boolean
+import { inject } from "vue";
+import { defineStore } from "pinia";
+import csv from "./Munddrik.tsv";
+export interface msg {
+  Message: string;
+  AllMessage: string[];
+  Munddrik: string[];
+  first: boolean;
 }
 const $papa = inject("$papa");
 export const useMunddrikStore = defineStore({
@@ -14,29 +14,27 @@ export const useMunddrikStore = defineStore({
     Message: "",
     AllMessage: [],
     Munddrik: ["Bund din drik!!!!", "Du vælger en som skal bunde!!!!"],
-    first: true
+    first: true,
   }),
-  actions:{
-    async loadMovies(){
-      await fetch('./Munddrik.txt').then(x => x.text)
-      csv.forEach(element => {
-        this.AllMessage.push(Object.values(element).toString())
+  actions: {
+    async loadMovies() {
+      await fetch("./Munddrik.txt").then((x) => x.text);
+      csv.forEach((element) => {
+        this.AllMessage.push(Object.values(element).toString());
       });
-      const valuest = Object.values(csv)
-      console.log(this.AllMessage)
-      if(this.first){
-        this.roll()
+      const valuest = Object.values(csv);
+      if (this.first) {
+        this.roll();
         this.first = false;
       }
     },
-    roll(){
-      const rand = Math.round((Math.random()*this.AllMessage.length));
-      this.Message = this.AllMessage[rand]
+    roll() {
+      const rand = Math.round(Math.random() * this.AllMessage.length);
+      this.Message = this.AllMessage[rand];
     },
-    haeld(){
-      let rand = Math.round((Math.random()*100)%2);
+    haeld() {
+      let rand = Math.round((Math.random() * 100) % 2);
       this.Message = this.Munddrik[rand ? 0 : 1];
     },
   },
-
-})
+});
